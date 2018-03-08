b6ccadabe5718d8e5e3abbc5c882841baa6ea017
Joan Marie Verba <verba001@umn.edu>
2018-03-08 15:44:02 -0600

    assign random numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,25 @@
 "use strict"
 
+//create function to get a random number between min and max, inclusive
+function getRandomInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
 
 let targetValue = 0;
 let currentScore = 0;
 let numberOfWins = 0;
 let numberOfLosses = 0;
+let blue = 0;
+let diamond = 0;
+let purple = 0;
+let yellow = 0;
 
+// assign random numbers to target and crystals
+targetValue = getRandomInclusive(19, 120);
+blue = getRandomInclusive(1, 12);
+diamond = getRandomInclusive(1, 12);
+purple = getRandomInclusive(1, 12);
+yellow = getRandomInclusive(1, 12);
 
 $("#target").text(targetValue);
 $("#score").text(currentScore);
