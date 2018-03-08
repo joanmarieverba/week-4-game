1f57d5f2c9bccbd5c6320d0726295283f4d57e2f
Joan Marie Verba <verba001@umn.edu>
2018-03-08 16:17:08 -0600

    afternoon

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -9,10 +9,15 @@ let targetValue = 0;
 let currentScore = 0;
 let numberOfWins = 0;
 let numberOfLosses = 0;
+
 let blue = 0;
 let diamond = 0;
 let purple = 0;
 let yellow = 0;
+
+let done = false;
+let win = false;
+let lose = false;
 
 // assign random numbers to target and crystals
 targetValue = getRandomInclusive(19, 120);
@@ -28,26 +33,53 @@ $("#numwins").text(`Number of wins: ${numberOfWins}`);
 $("#numlosses").text(`Number of losses: ${numberOfLosses}`);
 
 
+
 //blue button clicked
 $(".btn1").on("click", function () {
+    console.log("currentscorebefore ", currentScore);
     currentScore += blue;
+    console.log("currentscoreafter ", currentScore);
     console.log("blue ", blue);
+    $("#score").text(currentScore);
+
 });
 
 //diamond button clicked
 $(".btn2").on("click", function () {
     currentScore += diamond;
     console.log("diamond ", diamond);
+    $("#score").text(currentScore);
+
 });
 
 //purple button clicked
 $(".btn3").on("click", function () {
     currentScore += purple;
     console.log("purple ", purple);
+    $("#score").text(currentScore);
+
 });
 
 //yellow button clicked
 $(".btn4").on("click", function () {
     currentScore += yellow;
     console.log("yellow ", yellow);
+    $("#score").text(currentScore);
+
 });
+
+
+if (currentScore = targetValue) {
+    win = true;
+    numberOfWins++;
+    $("#numwins").text(`Number of wins: ${numberOfWins}`);
+    done = true;
+}
+if (currentScore > targetValue) {
+    lose = true;
+    numberofLosses++;
+    $("#numlosses").text(`Number of losses: ${numberOfLosses}`);
+    done = true;
+}
+
+
