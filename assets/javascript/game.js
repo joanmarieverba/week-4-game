ab9909c95025af0f711a0c18f697c9f23d537b70
Joan Marie Verba <verba001@umn.edu>
2018-03-08 15:30:18 -0600

    setup

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,28 +1,14 @@
 "use strict"
 
 
-let character1 = {
-    hp: 120,
-    ap: 5,
-    cap: 11,
-}
-
-let character2 = {
-    hp: 100,
-    ap: 3,
-    cap: 15,
-}
-
-let character3 = {
-    hp: 150,
-    ap: 7,
-    cap: 17,
-}
-
-let character4 = {
-    hp: 180,
-    ap: 11,
-    cap: 19,
-}
+let targetValue = 0;
+let currentScore = 0;
+let numberOfWins = 0;
+let numberOfLosses = 0;
 
 
+$("#target").text(targetValue);
+$("#score").text(currentScore);
+
+$("#numwins").text(`Number of wins: ${numberOfWins}`);
+$("#numlosses").text(`Number of losses: ${numberOfLosses}`);
