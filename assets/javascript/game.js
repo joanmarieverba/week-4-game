a62dd5269f8778c899f7e9d59487a0a101e16978
Joan Marie Verba <verba001@umn.edu>
2018-03-08 15:13:36 -0600

    set up images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,28 @@
 "use strict"
 
 
- 
+let character1 = {
+    hp: 120,
+    ap: 5,
+    cap: 11,
+}
+
+let character2 = {
+    hp: 100,
+    ap: 3,
+    cap: 15,
+}
+
+let character3 = {
+    hp: 150,
+    ap: 7,
+    cap: 17,
+}
+
+let character4 = {
+    hp: 180,
+    ap: 11,
+    cap: 19,
+}
+
 
